e724bc1a018133260a913376386cb9ed14a988cb
Nobertobrown <kibonanorberth@gmail.com>
2024-04-10 04:03:04 +0300

    [refactor]: minor change in the payment stage

--- src/components/ui/PaymentDetailsCard.jsx
+++ src/components/ui/PaymentDetailsCard.jsx
@@ -5,7 +5,7 @@ const PaymentDetailsCard = ({pName, isLoading}) => {
   return (
     <div className="flex flex-col border self-stretch space-y-3 p-3 rounded-md">
       <p className="font-semibold">Please enter your {pName} number:</p>
-      <Input required type="tel" name={`phoneNo`} placeholder="0876543210" />
+      <Input required type="tel" name={`phoneNo`} placeholder="+255123456789" />
       <Button text="Complete Payment" type="submit" loading={isLoading} />
     </div>
   );
